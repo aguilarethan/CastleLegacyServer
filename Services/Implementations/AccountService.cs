474b9a1c0cff833649f43cef1b1403e2cd959b1a
aguilarethan <126291882+aguilarethan@users.noreply.github.com>
2023-10-31 13:27:26 -0600

    Services

---
+++ Services/Implementations/AccountService.cs
@@ -0,0 +1,57 @@
+﻿using Database.Model;
+using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Implementations
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class AccountService : IAccount
+    {
+        public int AddAccount(Account account)
+        {
+            int band = 0;
+
+            try
+            {
+                using (var context = new CastleLegacyEntities())
+                {
+                    context.Account.Add(account);
+                    context.SaveChanges();
+                }
+                band = 1;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al registrar la cuenta" + ex.Message);
+            }
+
+            return band;
+        }
+
+        public Account GetAccount(string username)
+        {
+            try
+            {
+
+                using (var context = new CastleLegacyEntities())
+                {
+                    var account = context.Account.FirstOrDefault(a => a.Username == username);
+                    return account;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al obtener la cuenta" + ex.Message);
+                return null;
+
+            }
+        }
+    }
+}
---
+++ Services/Interfaces/IAccount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Database.Model;
+
+namespace Services.Interfaces
+{
+    [ServiceContract]
+    public interface IAccount
+    {
+        [OperationContract]
+        int AddAccount(Account account);
+        [OperationContract]
+        Account GetAccount(string username);
+
+    }
+}
