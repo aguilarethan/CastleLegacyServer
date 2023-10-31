200765298a955387c994c048bd69c337e9544e43
aguilarethan <126291882+aguilarethan@users.noreply.github.com>
2023-10-31 13:23:41 -0600

    Host

---
+++ Host/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using Services.Implementations;
+using Database.Model;
+
+namespace Host
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost serviceHost = new ServiceHost(typeof(Services.Implementations.AccountService)))
+            {
+                
+                serviceHost.Open();
+
+                Console.WriteLine("Servidor WCF en ejecución. Presiona Enter para detenerlo.");
+                Console.ReadLine();
+                
+            }
+        }
+    }
+}
